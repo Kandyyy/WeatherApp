cbbe41ae95d177397ad6b06ac99bb9e68e9c5247
Kandyyy <kingkandy016@gmail.com>
2022-07-02 23:32:31 +0530

    Added forms

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,url_for
 from Weather import Weather
 
 app = Flask(__name__)
 weather = Weather(url='')
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def index():
+    if request.method == "POST":
+        city = request.form.get("city")
+        print(city)
     return render_template("index.html")
 
 app.run(debug=True)
