a1ecd244fed86cd9c623c505a122fbecb1a0cf74
Kandyyy <kingkandy016@gmail.com>
2022-07-03 00:00:57 +0530

    Fixed forms

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template,request,url_for
+from flask import Flask, redirect, render_template,request,url_for
 from Weather import Weather
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+import os
+
+class CityForm(FlaskForm):
+    city = StringField('city', validators=[DataRequired()])
+
 
 app = Flask(__name__)
-weather = Weather(url='')
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    if request.method == "POST":
-        city = request.form.get("city")
-        print(city)
-    return render_template("index.html")
+    form = CityForm()
+    if form.validate_on_submit():
+        print(form.city.data)
+        return redirect(url_for('index'))
+    return render_template("index.html",form=form)
 
 app.run(debug=True)
