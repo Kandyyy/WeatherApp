bb3c452c2d7c3bc1cbb001d0fcc853dd009fc7bf
Kandyyy <kingkandy016@gmail.com>
2022-07-05 15:37:48 +0530

    Added Working Cards

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, redirect, render_template,request,url_for
+from flask import Flask, make_response, redirect, render_template,request,url_for
 from Weather import Weather
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import os
 
 class CityForm(FlaskForm):
-    city = StringField('city', validators=[DataRequired()])
-
+    city = StringField('city')
+    submit = SubmitField("Search")
 
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
@@ -16,9 +16,11 @@ app.config['SECRET_KEY'] = SECRET_KEY
 @app.route("/",methods=['GET','POST'])
 def index():
     form = CityForm()
+    city = False
     if form.validate_on_submit():
-        print(form.city.data)
-        return redirect(url_for('index'))
-    return render_template("index.html",form=form)
+        city = form.city.data
+        form.city.data = ""
+    return render_template("index.html",form=form,city=city)
+
 
 app.run(debug=True)
